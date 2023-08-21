fb704ab0fa3788ab91f698e222563050ba6a504a
yxyx-github <dev_mw@vodafonemail.de>
2023-08-21 17:12:29 +0200

    primevue added

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import './assets/tailwind.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Tailwind from 'primevue/passthrough/tailwind'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +12,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, { unstyled: true, pt: Tailwind })
 
 app.mount('#app')
