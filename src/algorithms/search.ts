38d61570add71f1253afb1bbcca03cbe2d4f4648
yxyx-github <dev_mw@vodafonemail.de>
2023-08-22 13:11:21 +0200

    types for simulations added

---
+++ src/algorithms/search.ts
@@ -0,0 +1,7 @@
+import type { SimulationResult, SimulationStep } from '@/simulation'
+
+interface SearchSimulationStep extends SimulationStep {
+}
+
+interface SearchSimulationResult extends SimulationResult {
+}
