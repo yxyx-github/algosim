e22e3fcd870186fdc89f3bd5be4b9d8152c93a67
yxyx-github <dev_mw@vodafonemail.de>
2023-08-28 11:02:00 +0200

    component implementation for sort visalization added

---
+++ src/components/app/visualization/SortVisualization.vue
@@ -0,0 +1,38 @@
+<template>
+    <svg :viewBox="`0 0 ${width} ${height}`">
+        <rect
+                v-for="(value, index) in props.step.sortedValues"
+                :key="index"
+                :x="index"
+                :y="height - value"
+                width="1"
+                :height="value"
+                :fill="isHighlighted(index) ? props.highlightColor : props.neutralColor"
+        />
+    </svg>
+</template>
+
+<script setup lang="ts">
+import type { SortSimulationStep } from '@/algorithms/sort'
+import { computed } from 'vue'
+
+const props = withDefaults(defineProps<{
+    step: SortSimulationStep
+    neutralColor?: string
+    highlightColor?: string
+}>(), {
+    neutralColor: '#8b8b8b',
+    highlightColor: '#ff0000',
+})
+
+const maxValue = computed(() => Math.max(...props.step.sortedValues))
+const width = computed(() => props.step.sortedValues.length)
+const height = computed(() => maxValue.value)
+
+function isHighlighted(index: number): boolean {
+    return props.step.highlightedIndices.indexOf(index) !== -1
+}
+</script>
+
+<style scoped>
+</style>
