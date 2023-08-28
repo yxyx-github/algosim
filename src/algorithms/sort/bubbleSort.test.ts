01efa11ba9d228104aacc5959d0dc0143745df49
yxyx-github <dev_mw@vodafonemail.de>
2023-08-28 12:07:43 +0200

    test fixed

--- src/algorithms/sort/bubbleSort.test.ts
+++ src/algorithms/sort/bubbleSort.test.ts
@@ -7,12 +7,12 @@ describe('bubbleSort', () => {
         const input = [3, 4, 2, 7]
         const expected: SortSimulation = {
             steps: [
-                { sortedValues: [3, 4, 2, 7], highlightedIndices: [0, 1, 3] },
-                { sortedValues: [3, 2, 4, 7], highlightedIndices: [1, 2, 3] },
-                { sortedValues: [3, 2, 4, 7], highlightedIndices: [2, 3, 3] },
-                { sortedValues: [2, 3, 4, 7], highlightedIndices: [0, 1, 2] },
-                { sortedValues: [2, 3, 4, 7], highlightedIndices: [1, 2, 2] },
-                { sortedValues: [2, 3, 4, 7], highlightedIndices: [0, 1, 1] },
+                { sortedValues: [3, 4, 2, 7], highlightedIndices: [0, 1] },
+                { sortedValues: [3, 2, 4, 7], highlightedIndices: [1, 2] },
+                { sortedValues: [3, 2, 4, 7], highlightedIndices: [2, 3] },
+                { sortedValues: [2, 3, 4, 7], highlightedIndices: [0, 1] },
+                { sortedValues: [2, 3, 4, 7], highlightedIndices: [1, 2] },
+                { sortedValues: [2, 3, 4, 7], highlightedIndices: [0, 1] },
             ],
             result: { sortedValues: [2, 3, 4, 7] }
         }
