b1e951a0dbfc3b197a5c09538cd26f3e59ab48f7
yxyx-github <dev_mw@vodafonemail.de>
2023-08-30 20:09:00 +0200

    layout fixed

--- src/components/app/simulation/sort/SortSimulationForm.vue
+++ src/components/app/simulation/sort/SortSimulationForm.vue
@@ -1,9 +1,9 @@
 <template>
-    <Form @submit.prevent x-class="@container/form">
+    <Form @submit.prevent>
         <Dropdown v-model="values.algorithm" optionLabel="label" optionValue="value" :options="algorithms" placeholder="Select an algorithm"/>
 <!--        TODO: remove "true ||"-->
         <template v-if="true || values.algorithm !== undefined">
-            <Input label="Number of items:" x-class="@lg/form:flex-row @lg/form:bg-blue-300">
+            <Input label="Number of items:">
                 <InputNumber v-model="values.count" showButtons buttonLayout="horizontal" incrementButtonIcon="pi pi-plus" decrementButtonIcon="pi pi-minus"/>
             </Input>
         </template>
