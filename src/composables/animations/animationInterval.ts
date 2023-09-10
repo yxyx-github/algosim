9a612df013aa1c6bdd5f184f033d75cfc29c6a5d
yxyx-github <dev_mw@vodafonemail.de>
2023-09-10 19:41:29 +0200

    switch between implementations in composable

--- src/composables/animations/animationInterval.ts
+++ src/composables/animations/animationInterval.ts
@@ -1,10 +1,10 @@
 import { useTimeoutIntervalTimer } from '@/composables/animations/intervalTimer/timeoutIntervalTimer'
 import { useAnimationFrameIntervalTimer } from '@/composables/animations/intervalTimer/animationFrameIntervalTimer'
 
-export function useAnimationInterval(...args) {
+export function useAnimationInterval(cb: () => void, next: () => boolean, duration: number | (() => number)) {
     if (import.meta.env.VITE_PLAYBACK_IMPLEMENTATION === 'animation-frame') {
-        return useAnimationFrameIntervalTimer(...args)
+        return useAnimationFrameIntervalTimer(cb, next, duration)
     } else {
-        return useTimeoutIntervalTimer(...args)
+        return useTimeoutIntervalTimer(cb, next, duration)
     }
 }
