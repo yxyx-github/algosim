32daac236cf3f8505e5acdfcf5164cd86d3e25b0
Philip Berner <philipberner.berlin@gmail.com>
2023-09-11 11:07:38 +0200

    added description to SelectionSort

--- src/main/algorithms/sort/selectionSort.ts
+++ src/main/algorithms/sort/selectionSort.ts
@@ -50,6 +50,14 @@ export class SelectionSort implements SortAlgorithmImplementation {
     }
 
     description(): string {
-        return 'Selectionsort description'
+        return 'Selectionsort ist ein weiterer naiver Sortieralgorithmus, dessen Funktionsweise simpel ist. ' +
+            'Die Liste wird mehrfach durchlaufen. ' +
+            'Bei jeder Iteration wird das kleinste Element ermittelt, indem jedes Element mit dem bisherigen kleinsten Element verglichen wird. ' +
+            'Das kleinste Element wird anschließend an den Beginn der unsortierten Liste bewegt und als sortiert gekennzeichnet. ' +
+            'Es entsteht eine Unterteilung in eine sortierte und einen unsortierte Liste. ' +
+            'Wie zu erkennen ist, wird bei jedem Iterieren über die unsortierte Liste ein Element sortiert. ' +
+            'Somit muss die Liste für jedes jedes Element einmal durchlaufen werden. ' +
+            'Deshalb spricht man bei Selectionsort von einer Laufzeitkomplexität von O(n^2).' +
+            'Ein Vorteil von Selectionsort ist hingegen sein Speicherverbrauch in der Größenordnung O(1).'
     }
 }
