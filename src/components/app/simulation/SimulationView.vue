4093a0f9ce6439fe29ddfb983ae46483d963eae6
yxyx-github <dev_mw@vodafonemail.de>
2023-09-11 11:30:04 +0200

    minimize on keydown.esc

--- src/components/app/simulation/SimulationView.vue
+++ src/components/app/simulation/SimulationView.vue
@@ -1,5 +1,5 @@
 <template>
-    <FColumn :grow="true" class="bg-white" :class="{ 'fixed top-0 left-0 bottom-0 right-0 p-1': fullScreen }" :justifyItems="fullScreen ? 'end' : 'start'">
+    <FColumn :grow="true" class="bg-white" :class="{ 'fixed top-0 left-0 bottom-0 right-0 p-1': fullScreen }" :justifyItems="fullScreen ? 'end' : 'start'" @keydown.esc="fullScreen = false">
         <slot v-if="currentSimulationStep !== null" name="step" :index="playbackValue - 1" :stepData="currentSimulationStep"/>
         <PlaybackControl v-model="playbackValue" :min="1" :max="numberOfSteps" :label="createPlaybackLabel">
             <Button @click="toggleFullScreen" :icon="`pi pi-window-${fullScreen ? 'min' : 'max'}imize`" :aria-label="fullScreenButtonLabel" v-tooltip.top="fullScreenButtonLabel"/>
