c664e2191b1a7801f5fff88dcb3245d49c29ff7f
yxyx-github <dev_mw@vodafonemail.de>
2023-09-12 11:49:19 +0200

    graph form types added

---
+++ src/main/algorithms/search/graphForm/types.ts
@@ -0,0 +1,26 @@
+export type GraphForm = GraphFormItem[][]
+
+export type GraphFormItem = {
+    type: GraphFormItemType,
+    label: string,
+    coords: Coords,
+    connections: TRBL<boolean>,
+    highlight: TRBL<boolean>,
+}
+
+export type Coords = {
+    x: number,
+    y: number
+}
+
+export type TRBL<T> = {
+    top: T,
+    right: T,
+    bottom: T,
+    left: T,
+}
+
+export enum GraphFormItemType {
+    NODE,
+    EDGE,
+}
