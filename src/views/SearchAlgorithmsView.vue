2552ae763479e079612841fa7fa9f32c5dcea78a
yxyx-github <dev_mw@vodafonemail.de>
2023-09-12 11:28:14 +0200

    basic search layout implemented

--- src/views/SearchAlgorithmsView.vue
+++ src/views/SearchAlgorithmsView.vue
@@ -1,8 +1,31 @@
 <template>
-    SearchAlgorithmsView
+    <SimulationLayout>
+        <template #form>
+            Form
+        </template>
+        <template v-if="simulation !== null" #simulation>
+            <SimulationView :simulation="simulation">
+                <template #step="{ stepData }">
+                    SimulationStep
+                </template>
+            </SimulationView>
+        </template>
+        <template #description>
+            <TextViewer :text="description"/>
+        </template>
+    </SimulationLayout>
 </template>
 
 <script setup lang="ts">
+import SimulationView from '@/components/app/simulation/SimulationView.vue'
+import SimulationLayout from '@/components/app/simulation/SimulationLayout.vue'
+import TextViewer from '@/components/lib/TextViewer.vue'
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+import type { SearchSimulation } from '@/main/algorithms/search/types'
+
+const simulation: Ref<SearchSimulation | null> = ref(null)
+const description: Ref<string[]> = ref([])
 </script>
 
 <style scoped>
