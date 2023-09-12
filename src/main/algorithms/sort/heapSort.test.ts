bc209b9a220f3c329c532719051db4cee958665c
Philip Berner <philipberner.berlin@gmail.com>
2023-09-12 16:56:16 +0200

    implemented HeapSort and Test

--- src/main/algorithms/sort/heapSort.test.ts
+++ src/main/algorithms/sort/heapSort.test.ts
@@ -3,7 +3,7 @@ import type { SortSimulation } from '@/main/algorithms/sort/types'
 import type { ProgressHandler, TrackableProgress } from '@/main/progressTracker/types'
 import {HeapSort} from "@/main/algorithms/sort/heapSort";
 
-describe('BubbleSort', () => {
+describe('HeapSort', () => {
     const mockTracker: TrackableProgress = {
         init: (overall: number) => {},
         track: (current: number, overall?: number) => {},
@@ -16,19 +16,29 @@ describe('BubbleSort', () => {
         const expected: SortSimulation = {
             steps: [
                 { sortedValues: [3, 4, 2, 7], highlightedIndices: [] },
+                { sortedValues: [3, 4, 2, 7], highlightedIndices: [{ type: 'current', index: 1 }, { type: 'current', index: 3 }] },
+                { sortedValues: [3, 7, 2, 4], highlightedIndices: [{ type: 'current', index: 1 }, { type: 'current', index: 3 }] },
+                { sortedValues: [3, 7, 2, 4], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 1 }] },
+                { sortedValues: [7, 3, 2, 4], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 1 }] },
+                { sortedValues: [7, 3, 2, 4], highlightedIndices: [{ type: 'current', index: 1 }, { type: 'current', index: 3 }] },
+                { sortedValues: [7, 4, 2, 3], highlightedIndices: [{ type: 'current', index: 1 }, { type: 'current', index: 3 }] },
+                { sortedValues: [7, 4, 2, 3], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 3 }] },
+                { sortedValues: [3, 4, 2, 7], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 3 }] },
                 { sortedValues: [3, 4, 2, 7], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 1 }, { type: 'threshold', index: 3 }] },
-                { sortedValues: [3, 4, 2, 7], highlightedIndices: [{ type: 'current', index: 1 }, { type: 'current', index: 2 }, { type: 'threshold', index: 3 }] },
-                { sortedValues: [3, 2, 4, 7], highlightedIndices: [{ type: 'current', index: 1 }, { type: 'current', index: 2 }, { type: 'threshold', index: 3 }] },
-                { sortedValues: [3, 2, 4, 7], highlightedIndices: [{ type: 'current', index: 2 }, { type: 'current', index: 3 }] },
-                { sortedValues: [3, 2, 4, 7], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 1 }, { type: 'threshold', index: 2 }] },
-                { sortedValues: [2, 3, 4, 7], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 1 }, { type: 'threshold', index: 2 }] },
-                { sortedValues: [2, 3, 4, 7], highlightedIndices: [{ type: 'current', index: 1 }, { type: 'current', index: 2 }] },
-                { sortedValues: [2, 3, 4, 7], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 1 }] },
+                { sortedValues: [4, 3, 2, 7], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 1 },  { type: 'threshold', index: 3 }] },
+                { sortedValues: [4, 3, 2, 7], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 2 },  { type: 'threshold', index: 3 }] },
+                { sortedValues: [2, 3, 4, 7], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 2 },  { type: 'threshold', index: 3 }] },
+                { sortedValues: [2, 3, 4, 7], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 1 },  { type: 'threshold', index: 2 }] },
+                { sortedValues: [3, 2, 4, 7], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 1 },  { type: 'threshold', index: 2 }] },
+                { sortedValues: [3, 2, 4, 7], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 1 },  { type: 'threshold', index: 2 }] },
+                { sortedValues: [2, 3, 4, 7], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 1 },  { type: 'threshold', index: 2 }] },
+                { sortedValues: [2, 3, 4, 7], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'threshold', index: 1 }] },
+                { sortedValues: [2, 3, 4, 7], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'threshold', index: 1 }] },
                 { sortedValues: [2, 3, 4, 7], highlightedIndices: [] },
             ],
         }
 
-        const result = new BubbleSort().sort(input)
+        const result = new HeapSort().sort(input)
         expect(result).to.deep.equal(expected)
     })
 
@@ -37,14 +47,24 @@ describe('BubbleSort', () => {
         const expected: SortSimulation = {
             steps: [
                 { sortedValues: [3, 4, 2, 7], highlightedIndices: [] },
+                { sortedValues: [3, 4, 2, 7], highlightedIndices: [{ type: 'current', index: 1 }, { type: 'current', index: 3 }] },
+                { sortedValues: [3, 7, 2, 4], highlightedIndices: [{ type: 'current', index: 1 }, { type: 'current', index: 3 }] },
+                { sortedValues: [3, 7, 2, 4], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 1 }] },
+                { sortedValues: [7, 3, 2, 4], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 1 }] },
+                { sortedValues: [7, 3, 2, 4], highlightedIndices: [{ type: 'current', index: 1 }, { type: 'current', index: 3 }] },
+                { sortedValues: [7, 4, 2, 3], highlightedIndices: [{ type: 'current', index: 1 }, { type: 'current', index: 3 }] },
+                { sortedValues: [7, 4, 2, 3], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 3 }] },
+                { sortedValues: [3, 4, 2, 7], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 3 }] },
                 { sortedValues: [3, 4, 2, 7], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 1 }, { type: 'threshold', index: 3 }] },
-                { sortedValues: [3, 4, 2, 7], highlightedIndices: [{ type: 'current', index: 1 }, { type: 'current', index: 2 }, { type: 'threshold', index: 3 }] },
-                { sortedValues: [3, 2, 4, 7], highlightedIndices: [{ type: 'current', index: 1 }, { type: 'current', index: 2 }, { type: 'threshold', index: 3 }] },
-                { sortedValues: [3, 2, 4, 7], highlightedIndices: [{ type: 'current', index: 2 }, { type: 'current', index: 3 }] },
-                { sortedValues: [3, 2, 4, 7], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 1 }, { type: 'threshold', index: 2 }] },
-                { sortedValues: [2, 3, 4, 7], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 1 }, { type: 'threshold', index: 2 }] },
-                { sortedValues: [2, 3, 4, 7], highlightedIndices: [{ type: 'current', index: 1 }, { type: 'current', index: 2 }] },
-                { sortedValues: [2, 3, 4, 7], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 1 }] },
+                { sortedValues: [4, 3, 2, 7], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 1 },  { type: 'threshold', index: 3 }] },
+                { sortedValues: [4, 3, 2, 7], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 2 },  { type: 'threshold', index: 3 }] },
+                { sortedValues: [2, 3, 4, 7], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 2 },  { type: 'threshold', index: 3 }] },
+                { sortedValues: [2, 3, 4, 7], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 1 },  { type: 'threshold', index: 2 }] },
+                { sortedValues: [3, 2, 4, 7], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 1 },  { type: 'threshold', index: 2 }] },
+                { sortedValues: [3, 2, 4, 7], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 1 },  { type: 'threshold', index: 2 }] },
+                { sortedValues: [2, 3, 4, 7], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'current', index: 1 },  { type: 'threshold', index: 2 }] },
+                { sortedValues: [2, 3, 4, 7], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'threshold', index: 1 }] },
+                { sortedValues: [2, 3, 4, 7], highlightedIndices: [{ type: 'current', index: 0 }, { type: 'threshold', index: 1 }] },
                 { sortedValues: [2, 3, 4, 7], highlightedIndices: [] },
             ],
         }
@@ -57,8 +77,8 @@ describe('BubbleSort', () => {
         expect(result).to.deep.equal(expected)
 
         expect(spyInit).toHaveBeenCalledOnce()
-        expect(spyInit).toHaveBeenCalledWith(6)
+        expect(spyInit).toHaveBeenCalledWith(4)
         expect(spyTrack).toHaveBeenCalledTimes(0)
-        expect(spyTrackNext).toHaveBeenCalledTimes(6)
+        expect(spyTrackNext).toHaveBeenCalledTimes(4)
     })
 })
--- src/main/algorithms/sort/heapSort.ts
+++ src/main/algorithms/sort/heapSort.ts
@@ -5,17 +5,89 @@ import type { TrackableProgress } from '@/main/progressTracker/types'
 export class HeapSort implements SortAlgorithmImplementation {
 
     sort(numbers: number[], progressTracker?: TrackableProgress): SortSimulation {
-        progressTracker?.init(1)
+        progressTracker?.init(numbers.length)
         const pB = new ProtocolBuilder<SortSimulationStep>()
         pB.step({
             sortedValues: numbers,
             highlightedIndices: [],
         })
+
+        for (let i = Math.floor(numbers.length / 2) - 1; i >= 0; i--) {
+            this.heapify(numbers, numbers.length, i, pB, progressTracker);
+        }
+
+        for (let i = numbers.length - 1; i >= 0; i--) {
+            progressTracker?.trackNext()
+            pB.step({
+                sortedValues: numbers,
+                highlightedIndices: [
+                    { type: 'current', index: 0 },
+                    ...(i === 0 ? [] : [{ type: 'current', index: i }]),
+                    ...(i + 1 === numbers.length ? [] : [{ type: 'threshold', index: i + 1 }]),
+
+                ] as HighlightedIndex[],
+            })
+            let temp = numbers[0];
+            numbers[0] = numbers[i];
+            numbers[i] = temp;
+
+            pB.step({
+                sortedValues: numbers,
+                highlightedIndices: [
+                    { type: 'current', index: 0 },
+                    ...(i === 0 ? [] : [{ type: 'current', index: i }]),
+                    ...(i + 1 === numbers.length ? [] : [{ type: 'threshold', index: i + 1 }]),
+                ] as HighlightedIndex[],
+            })
+
+            this.heapify(numbers, i, 0, pB, progressTracker);
+        }
+
         pB.step({
             sortedValues: numbers,
             highlightedIndices: [],
         })
         return pB.build()
+    }
+
+    private heapify(numbers: number[], n: number, i: number, pB: ProtocolBuilder<SortSimulationStep>, progressTracker?: TrackableProgress) {
+        let largest = i;
+        let l = 2 * i + 1;
+        let r = 2 * i + 2;
+
+        if (l < n && numbers[l] > numbers[largest]) {
+            largest = l;
+        }
+
+        if (r < n && numbers[r] > numbers[largest]) {
+            largest = r;
+        }
+
+        if (largest != i) {
+            pB.step({
+                sortedValues: numbers,
+                highlightedIndices: [
+                    { type: 'current', index: i },
+                    { type: 'current', index: largest },
+                    ...(n === numbers.length ? [] : [{ type: 'threshold', index: n }]),
+                ] as HighlightedIndex[],
+            })
+
+            let swap = numbers[i];
+            numbers[i] = numbers[largest];
+            numbers[largest] = swap;
+
+            pB.step({
+                sortedValues: numbers,
+                highlightedIndices: [
+                    { type: 'current', index: i },
+                    { type: 'current', index: largest },
+                    ...(n === numbers.length ? [] : [{ type: 'threshold', index: n }]),
+                ] as HighlightedIndex[],
+            })
+
+            this.heapify(numbers, n, largest, pB, progressTracker);
+        }
     }
 
     description(): string {
