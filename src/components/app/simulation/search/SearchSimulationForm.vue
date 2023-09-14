02197edac296f3d03ded466ff234fd258686b917
yxyx-github <dev_mw@vodafonemail.de>
2023-09-14 13:30:39 +0200

    clear layout fixed

--- src/components/app/simulation/search/SearchSimulationForm.vue
+++ src/components/app/simulation/search/SearchSimulationForm.vue
@@ -1,14 +1,16 @@
 <template>
     <Form @submit.prevent="submit">
-        <div :class="`grid grid-rows-[1fr_2rem] grid-cols-[1fr_2rem] gap-2 max-w-fit max-h-min`">
+        <div class="grid gap-2 max-w-fit max-h-min" :class="isEmpty ? 'grid-rows-[2rem_2rem] grid-cols-[2rem_2rem]' : 'grid-rows-[1fr_2rem] grid-cols-[1fr_2rem]'">
             <div
                     class="grid gap-0 max-w-fit max-h-min cursor-crosshair"
                     :style="`grid-template-columns: repeat(${cols}, minmax(0, 1fr));`"
             >
-                <GraphFormItemVisualization v-for="(item, index) in graphFormItems" :key="index" class="w-full h-full" :item="item"/>
+                <div v-if="isEmpty"></div>
+                <GraphFormItemVisualization v-else v-for="(item, index) in graphFormItems" :key="index" class="w-full h-full" :item="item"/>
             </div>
-            <Button @click="addColumn" class="w-[2rem]" icon="pi pi-plus" severity="secondary"/>
-            <Button @click="addRow" class="w-full" icon="pi pi-plus" severity="secondary"/>
+            <Button @click="addColumn" class="w-[2rem]" icon="pi pi-plus" severity="secondary" aria-label="Add Column" v-tooltip.top="'Add Column'"/>
+            <Button @click="addRow" class="w-full" icon="pi pi-plus" severity="secondary" aria-label="Add Row" v-tooltip.top="'Add Row'"/>
+            <Button @click="clear" class="w-full" icon="pi pi-trash" severity="danger" aria-label="Clear" v-tooltip.top="'Clear'"/>
         </div>
     </Form>
 </template>
@@ -81,7 +83,8 @@ const graphFormItems: ComputedRef<GraphFormItem[]> = computed(() => {
 })
 
 const rows = computed(() => graphForm.value.length)
-const cols = computed(() => graphForm.value[0].length)
+const cols = computed(() => graphForm.value[0]?.length ?? 0)
+const isEmpty = computed(() => rows.value === 0 && cols.value === 0)
 
 function addRow() {
     const oldCols = cols.value
@@ -110,6 +113,10 @@ function addColumn() {
         connect: { top: false, right: false, bottom: false, left: false },
         highlight: { top: false, right: false, bottom: false, left: false },
     }))
+}
+
+function clear() {
+    graphForm.value = []
 }
 
 function submit() {
