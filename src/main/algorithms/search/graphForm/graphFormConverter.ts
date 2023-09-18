47262edd1a8b36ead35a5d2d63788f3d2ebc716d
yxyx-github <dev_mw@vodafonemail.de>
2023-09-18 12:03:34 +0200

    GraphFormConverter: filter blank items

--- src/main/algorithms/search/graphForm/graphFormConverter.ts
+++ src/main/algorithms/search/graphForm/graphFormConverter.ts
@@ -1,14 +1,17 @@
 import type { EdgeValue, GraphForm, VertexValue } from '@/main/algorithms/search/graphForm/types'
 import { Graph } from '@/main/algorithms/search/graph/graph'
+import { graphFormToItems } from '@/main/algorithms/search/graphForm/index'
 
 export class GraphFormConverter {
     graphFromForm(form: GraphForm): Graph<VertexValue, EdgeValue> {
         console.log('convert:', form)
         const graph = new Graph<VertexValue, EdgeValue>()
 
-        form.forEach(item => {
-            // TODO: filter blank items
-        })
+        graphFormToItems(form)
+            .filter(item => Object.values(item.connections).some(v => v))
+            .forEach(item => {
+                console.log(item)
+            })
 
         return graph
     }
