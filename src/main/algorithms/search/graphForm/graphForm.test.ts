3e650912d86fd6cd2269a0508d42ef57636ac798
yxyx-github <dev_mw@vodafonemail.de>
2023-09-20 09:39:14 +0200

    GraphForm and GraphFormItem test files added

---
+++ src/main/algorithms/search/graphForm/graphForm.test.ts
@@ -0,0 +1,6 @@
+import { describe, expect, test } from 'vitest'
+
+describe('GraphForm', () => {
+    test('can create GraphForm', () => {
+    })
+})
---
+++ src/main/algorithms/search/graphForm/graphFormItem.test.ts
@@ -0,0 +1,6 @@
+import { describe, expect, test } from 'vitest'
+
+describe('GraphFormItem', () => {
+    test('can create GraphFormItem', () => {
+    })
+})
