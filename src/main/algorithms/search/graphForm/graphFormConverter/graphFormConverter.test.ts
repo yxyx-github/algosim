7d90a71f6ce5773a62fdff5804bd09a033af8c34
yxyx-github <dev_mw@vodafonemail.de>
2023-09-26 10:58:42 +0200

    test: convert GraphForm to multi graph with 3 edges between two vertices, still need to fix multiple edges with the same weight

--- src/main/algorithms/search/graphForm/graphFormConverter/graphFormConverter.test.ts
+++ src/main/algorithms/search/graphForm/graphFormConverter/graphFormConverter.test.ts
@@ -33,7 +33,8 @@ describe('GraphFormConverter', () => {
         graph.getEdges().forEach(edge => {
             const expectedEdge = expectedGraph.findEdge((e: Edge<VertexValue, EdgeValue>) =>
                 e.getTo().getId() === edge.getTo().getId() &&
-                e.getFrom().getId() === edge.getFrom().getId()
+                e.getFrom().getId() === edge.getFrom().getId() &&
+                e.getWeight() === edge.getWeight()
             )
 
             expect(edge.getFrom()).to.deep.equal(expectedEdge?.getFrom())
@@ -363,5 +364,54 @@ describe('GraphFormConverter', () => {
         assertGraph(converter.toGraph(), expectedGraph)
     })
 
-    // TODO: test 3 edges between two vertices
+    test('can convert GraphForm to multi graph with 3 edges between two vertices', () => {
+        const item00 = getItemWithConnections({ x: 0, y: 0, }, { top: false, right: true, bottom: true, left: false })
+        const item10 = getItemWithConnections({ x: 1, y: 0, }, { top: false, right: true, bottom: false, left: true })
+        const item20 = getItemWithConnections({ x: 2, y: 0, }, { top: false, right: false, bottom: true, left: true })
+        const item01 = getItemWithConnections({ x: 0, y: 1, }, { top: true, right: true, bottom: true, left: false })
+        const item11 = getItemWithConnections({ x: 1, y: 1, }, { top: false, right: true, bottom: false, left: true })
+        const item21 = getItemWithConnections({ x: 2, y: 1, }, { top: true, right: false, bottom: true, left: true })
+        const item02 = getItemWithConnections({ x: 0, y: 2, }, { top: true, right: true, bottom: false, left: false })
+        const item12 = getItemWithConnections({ x: 1, y: 2, }, { top: false, right: true, bottom: false, left: true })
+        const item22 = getItemWithConnections({ x: 2, y: 2, }, { top: true, right: false, bottom: false, left: true })
+
+        const expectedGraph = new Graph<VertexValue, EdgeValue>()
+        const v1 = new Vertex<VertexValue>('x:0y:1', {
+            item: item01,
+        })
+        expectedGraph.addVertex(v1)
+        const v2 = new Vertex<VertexValue>('x:2y:1', {
+            item: item21,
+        })
+        expectedGraph.addVertex(v2)
+        expectedGraph.addEdgeBetween(v1, v2, 3, {
+            items: [item00, item10, item20],
+        })
+        expectedGraph.addEdgeBetween(v1, v2, 1, {
+            items: [item11],
+        })
+        /*expectedGraph.addEdgeBetween(v1, v2, 3, { // TODO: implement multi graph that allows two edges with the same weight
+            items: [item02, item12, item22],
+        })*/
+
+        const graphForm = new GraphForm()
+        graphForm.addRow()
+        graphForm.addColumn()
+        graphForm.addRow()
+        graphForm.addColumn()
+
+        graphForm.updateItem(item00)
+        graphForm.updateItem(item10)
+        graphForm.updateItem(item20)
+        graphForm.updateItem(item01)
+        graphForm.updateItem(item11)
+        graphForm.updateItem(item21)
+        graphForm.updateItem(item02)
+        graphForm.updateItem(item12)
+        graphForm.updateItem(item22)
+
+        const converter = new GraphFormConverter(graphForm)
+
+        assertGraph(converter.toGraph(), expectedGraph)
+    })
 })
