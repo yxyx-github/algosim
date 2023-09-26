8e13444337db0c960b3403642f695054bde0f879
yxyx-github <dev_mw@vodafonemail.de>
2023-09-26 11:15:26 +0200

    GraphFormInput: buttons to add rows and cols on opposite sides added

--- src/components/app/simulation/search/GraphFormInput.vue
+++ src/components/app/simulation/search/GraphFormInput.vue
@@ -1,5 +1,9 @@
 <template>
-    <div class="grid gap-2 max-w-fit max-h-min" :class="isEmpty ? 'grid-rows-[2rem_2rem] grid-cols-[2rem_2rem]' : 'grid-rows-[1fr_2rem] grid-cols-[1fr_2rem]'">
+    <div class="grid gap-2 max-w-fit max-h-min" :class="isEmpty ? 'grid-rows-[2rem_2rem_2rem] grid-cols-[2rem_2rem_2rem]' : 'grid-rows-[2rem_1fr_2rem] grid-cols-[2rem_1fr_2rem]'">
+        <div></div>
+        <Button @click="addFirstRow" class="w-full" icon="pi pi-plus" severity="secondary" aria-label="Add Row" v-tooltip.top="'Add Row'"/>
+        <div></div>
+        <Button @click="addFirstColumn" class="w-[2rem]" icon="pi pi-plus" severity="secondary" aria-label="Add Column" v-tooltip.top="'Add Column'"/>
         <div
                 class="grid gap-0 max-w-fit max-h-min cursor-crosshair"
                 :style="`grid-template-columns: repeat(${cols}, minmax(0, 1fr));`"
@@ -8,6 +12,7 @@
             <GraphFormItemVisualization v-else v-for="(item, index) in graphFormItems" :key="index" class="w-full h-full" :item="item"/>
         </div>
         <Button @click="addColumn" class="w-[2rem]" icon="pi pi-plus" severity="secondary" aria-label="Add Column" v-tooltip.top="'Add Column'"/>
+        <div></div>
         <Button @click="addRow" class="w-full" icon="pi pi-plus" severity="secondary" aria-label="Add Row" v-tooltip.top="'Add Row'"/>
         <Button @click="clear" class="w-full" icon="pi pi-trash" severity="danger" aria-label="Clear" v-tooltip.top="'Clear'"/>
     </div>
@@ -29,6 +34,14 @@ const graphFormItems = computed(() => props.graphForm.toItems())
 
 const cols = computed(() => props.graphForm.cols())
 const isEmpty = computed(() => props.graphForm.isEmpty())
+
+function addFirstRow() {
+    console.log('addFirstRow')
+}
+
+function addFirstColumn() {
+    console.log('addFirstColumn')
+}
 
 function addRow() {
     props.graphForm.addRow()
