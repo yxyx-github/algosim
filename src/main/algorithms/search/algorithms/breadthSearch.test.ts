43e89c33dcd1743f8e60e33cd5289412fab283b9
yxyx-github <dev_mw@vodafonemail.de>
2023-10-02 11:38:42 +0200

    fix test name

--- src/main/algorithms/search/algorithms/breadthSearch.test.ts
+++ src/main/algorithms/search/algorithms/breadthSearch.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, test, vi } from 'vitest'
 import type { ProgressHandler, TrackableProgress } from '@/main/progressTracker/types'
 
-describe('BubbleSort', () => {
+describe('BreadthSearch', () => {
     const mockTracker: TrackableProgress = {
         init: (overall: number) => {},
         track: (current: number, overall?: number) => {},
