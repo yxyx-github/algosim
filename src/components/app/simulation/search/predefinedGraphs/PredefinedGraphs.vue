c7523dea0ed6e700bebf5818ecc529f727cd0f69
yxyx-github <dev_mw@vodafonemail.de>
2023-10-05 15:27:09 +0200

    empty PredefinedGraphs modal added

---
+++ src/components/app/simulation/search/predefinedGraphs/PredefinedGraphs.vue
@@ -0,0 +1,26 @@
+<template>
+    <Dialog v-model:visible="visible" modal header="Select a graph" :style="{ 'min-width': '80vw' }" contentClass="mb:w-screen md:w-full">
+        PredefinedGraphs
+    </Dialog>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import Dialog from 'primevue/dialog'
+
+const emit = defineEmits<{
+    'update:show': [value: boolean],
+}>()
+
+const props = defineProps<{
+    show: boolean
+}>()
+
+const visible = computed({
+    get: () => props.show,
+    set: val => emit('update:show', val),
+})
+</script>
+
+<style scoped>
+</style>
