9e726e695912be478cc34d2830bd63b513521df8
yxyx-github <dev_mw@vodafonemail.de>
2023-10-05 18:37:29 +0200

    wip: aspect-ratio does not work

--- src/components/app/simulation/search/predefinedGraphs/PredefinedGraph.vue
+++ src/components/app/simulation/search/predefinedGraphs/PredefinedGraph.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="max-w-72 h-72 border-4 border-red-500">
-        <GraphFormVisualization class="h-72 border-4 border-yellow-500" :graphForm="props.graphForm"/>
+        <GraphFormVisualization class="w-72 h-72 border-4 border-yellow-500" :graphForm="props.graphForm"/>
     </div>
 </template>
 
