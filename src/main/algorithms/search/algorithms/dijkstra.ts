6668f8a97f3352e87db2aa75a487b4bcb252ca13
yxyx-github <dev_mw@vodafonemail.de>
2023-10-06 09:50:47 +0200

    extract DijkstraQueue into separate file

--- src/main/algorithms/search/algorithms/dijkstra.ts
+++ src/main/algorithms/search/algorithms/dijkstra.ts
@@ -10,6 +10,13 @@ import { ProtocolBuilder } from '@/main/simulation/protocolBuilder'
 import { GraphFormItem } from '@/main/algorithms/search/graphForm/graphFormItem'
 import { cloneGrid, cloneSearchSimulationStep } from '@/main/algorithms/search/algorithms/index'
 import type { Edge } from '@/main/algorithms/search/graph/edge'
+import { DijkstraQueue } from '@/main/dataStructures/DijkstraQueue'
+
+export interface VertexDijkstraValue extends VertexValue {
+    completed?: boolean
+    distance?: number
+    predecessor?: Edge<VertexDijkstraValue, EdgeValue>
+}
 
 export class Dijkstra implements SearchAlgorithmImplementation {
 
@@ -171,47 +178,5 @@ export class Dijkstra implements SearchAlgorithmImplementation {
         return [`
             Dijkstra description
         `]
-    }
-}
-
-interface VertexDijkstraValue extends VertexValue {
-    completed?: boolean
-    distance?: number
-    predecessor?: Edge<VertexDijkstraValue, EdgeValue>
-}
-
-class DijkstraQueue {
-
-    private readonly vertices: Vertex<VertexDijkstraValue>[]
-
-    constructor(vertices: Vertex<VertexDijkstraValue>[]) {
-        this.vertices = Array<Vertex<VertexDijkstraValue>>(...vertices)
-    }
-
-    public offer(element: Vertex<VertexDijkstraValue>) {
-        this.vertices.push(element)
-    }
-
-    public poll() {
-        if (this.isEmpty()) {
-            return null
-        }
-        let min = 0
-        this.vertices.forEach((val, i) => {
-            if ((val.getValue().distance ?? Infinity) < (this.vertices[min].getValue().distance ?? Infinity)) {
-                min = i
-            }
-        })
-        let res = this.vertices[min]
-        this.vertices.splice(min, 1)
-        return res
-    }
-
-    public isEmpty(): boolean {
-        return this.size() == 0
-    }
-
-    public size(): number {
-        return this.vertices.length
     }
 }
